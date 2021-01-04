75f93ce673945046743031656099b8925edd7668
Ray Catley <ray.catley@smartmt.com>
2021-01-04 11:24:36 +0000

    Added test to show that PureMethods in other assemblies do not have that purity information used at build time analysis. I.e. implementation of rule CA1806 does not ensure dependent assemblies are using pure method results

---
+++ CrossAssemblyAnalysisMissing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using PureAssembly;
+
+namespace CrossAssemblyAnalysisMissing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //without the discard, the following line will cause the build to fail as expected.
+            var _ = PureMethod();
+
+            //the following line should also require a discard:
+            HasNoState.PureMethod();
+            Console.WriteLine("Test failed! This should not compile as CA1806 has a severity of error.");
+        }
+
+        [Pure]
+        public static int PureMethod()
+        {
+            return 101;
+        }
+    }
+}
---
+++ PureAssembly/HasNoState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace PureAssembly
+{
+    public static class HasNoState
+    {
+        [Pure]
+        public static int PureMethod()
+        {
+            return 101;
+        }
+    }
+}
